0ae9dd43932ae313aca24de0be047de16f4d4b92
Camila1953 <ayelenchavez1953@gmail.com>
2024-07-16 14:42:34 -0300

    corrección de identación en las líneas 5 y 15

--- src/02-js/04-reducir/index.js
+++ src/02-js/04-reducir/index.js
@@ -2,7 +2,7 @@ function calcOne(){
     const entryNumbers = [456, 234, 78, 2, 8, 90, -12, 4, -23, 0, -100, 50];
 
     let sum = entryNumbers.reduce((total, num) => total + num);
-        average = sum / entryNumbers.length;
+    let average = sum / entryNumbers.length;
 
     console.log('La sumatoria de todos los valores es de',sum, 'y su pormedio es de', average);
 }
@@ -12,7 +12,7 @@ function calcTwo(){
     const entryStrings = ['asd', '123', '0a24', '1', '!', '_hola', '-6d', 'ASD', "??"];
 
     let concatenation = entryStrings.reduce((total, word) => total + word);
-        elements = entryStrings.length;
+    let elements = entryStrings.length;
 
     console.log(`Concatenación de todos los elementos: ${concatenation}, cantidad de elementos que posee el array:`, elements);
 }
